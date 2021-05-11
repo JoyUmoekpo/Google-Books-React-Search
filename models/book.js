70673f3b31d3a383d0e311db0c60320721056c06
Joy <joyumoekpo1@gmail.com>
2021-05-11 06:05:00 -0500

    Modified models books.js file

--- models/book.js
+++ models/book.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  author: { type: [String], required: true },  
+  description: { type: String },
+  image: { type: String },
+  link: { type: String },
   synopsis: String,
   date: { type: Date, default: Date.now }
 });
